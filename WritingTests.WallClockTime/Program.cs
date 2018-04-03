7d960a4fd35e4aa269b28c8efcd37af13a290ae4
Sander Saares <saares@axinom.com>
2018-04-03 17:12:50 +0300

    Add project files.

---
+++ WritingTests.WallClockTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WritingTests.WallClockTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
